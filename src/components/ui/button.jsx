20ea36572fd88dffe314499888b3ce5909b7eba7
guimazaky <guimazakiyama@gmail.com>
2025-04-05 20:03:29 -0300

    button-hover

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default:
-          "bg-[#FC4B08]/70 rounded-full text-primary-foreground shadow-xs hover:bg-primary/90",
+          "bg-[#FC4B08]/70 rounded-full text-primary-foreground shadow-xs hover:bg-[#15015F] cursor-pointer ",
         destructive:
           "bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
         outline:
--- src/components/ui/navbar.jsx
+++ src/components/ui/navbar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 import { CircleUserRound } from "lucide-react";
+import { Button } from "./button";
 
 function Navbar() {
   return (
     <div className="mx-16 h-28 border-b-1 border-white/20 flex justify-between items-center ">
       <h1 className="text-white font-didot text-5xl">Vessel</h1>
       <div className="text-white  mr-12 flex gap-8">
-        <button onClick={"/home"} className="text-2xl">
+        <Button variant="default" className=" w-20 h-8 text-1xl text-white">
           Login
-        </button>
+        </Button>
+
         <a href="">
           <CircleUserRound className="w-8 h-8" />
         </a>
