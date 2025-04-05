a85379ca3be049fcb7b63b85bf6a9bac8fe6906f
guimazaky <guimazakiyama@gmail.com>
2025-04-05 20:14:12 -0300

    create-path-loadingpage

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import LoadingPage from "./pages/LoadingPage";
 import Home from "./pages/Home";
+import LoginPage from "./pages/LoginPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Routes>
       <Route path="/" element={<LoadingPage />} />
       <Route path="/home" element={<Home />} />
+      <Route path="/loginpage" element={<LoginPage />} />
     </Routes>
   </Router>
 );
